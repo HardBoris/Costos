2d1b75180dcbec67fc7eae09ced1de43f6f5f2ba
Boris Gaibor <bgaibor03@gmail.com>
2023-02-08 20:23:02 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+export default function App() {
+  return (
+    <>
+      <header>
+        <h1>Oikos</h1>
+      </header>
+      <main>
+        React ⚛️ + Vite ⚡ + Replit 🌀
+      </main>
+      <footer>
+        <h4>Powered by BG</h4>
+      </footer>
+    </>
+  )
+}
